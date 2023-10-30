4ae34fbde7c151f1bee80e37b9f46a6f96a4b4e3
Timur Mendgaliev <39248041+timurmendgaliev@users.noreply.github.com>
2023-10-30 17:51:49 +0300

    Add files via upload

---
+++ individual_task/datatable_creation.sql
@@ -0,0 +1,10 @@
+CREATE TABLE user_actions (
+    id serial PRIMARY KEY,
+    user_id INT,
+    session_id INT,
+    action_id INT,
+    action_date TIMESTAMP,
+    element VARCHAR(20),
+    profit INT,
+    platform VARCHAR(20)
+);
