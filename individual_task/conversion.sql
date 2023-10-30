59c1b2b0a3aa7456e338db85731591a640654195
Timur Mendgaliev <39248041+timurmendgaliev@users.noreply.github.com>
2023-10-30 18:06:39 +0300

    Delete individual_task/conversion.sql

--- individual_task/conversion.sql
+++
@@ -1,7 +0,0 @@
-select 
-  platform,
-  COUNT(DISTINCT action_id) as total_actions,
-  COUNT(DISTINCT action_id) FILTER (WHERE profit > 0) AS successful_actions,
-  (COUNT(DISTINCT action_id) FILTER (WHERE profit > 0)::NUMERIC / COUNT(DISTINCT action_id)) AS conversion
-FROM user_actions
-GROUP BY platform
